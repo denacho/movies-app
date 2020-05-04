01926d9a58ec8e8fa66c060091c272cd5af5c4d2
Dena Cho <denaexplore@gmail.com>
2020-05-04 23:34:29 +0200

    Initial commit

---
+++ build/precache-manifest.6600c5249aff7f5b788478b0d9589c0b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b7b583b2af1011c4236242e9e22b5cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c1e103015a477f481970",
+    "url": "/static/css/2.6c02c5f5.chunk.css"
+  },
+  {
+    "revision": "dc96a9cffb5b3e8e21cb",
+    "url": "/static/css/main.1f2d14a6.chunk.css"
+  },
+  {
+    "revision": "c1e103015a477f481970",
+    "url": "/static/js/2.6612d321.chunk.js"
+  },
+  {
+    "revision": "24cc20256b844955ea8cdd54482d0b64",
+    "url": "/static/js/2.6612d321.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc96a9cffb5b3e8e21cb",
+    "url": "/static/js/main.8858a980.chunk.js"
+  },
+  {
+    "revision": "9bd7a91592a3826eb507",
+    "url": "/static/js/runtime-main.f3676756.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React, { Component } from "react";
+import { Route, Redirect, Switch } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import Movies from "./component/movies";
+import MovieForm from "./component/movieForm";
+import Customers from "./component/customers";
+import Rentals from "./component/rentals";
+import NotFound from "./component/notFound";
+import NavBar from "./component/navBar";
+import RegisterForm from "./component/registerForm";
+import LoginForm from "./component/loginForm";
+import Logout from "./component/logout";
+import ProtectedRoute from "./component/common/protectedRoute";
+import auth from "./services/authService";
+import "react-toastify/dist/ReactToastify.css";
+import "./App.css";
+
+class App extends Component {
+  state = {};
+
+  componentDidMount() {
+    const user = auth.getCurrentUser();
+    this.setState({ user });
+  }
+
+  render() {
+    const { user } = this.state;
+
+    return (
+      <React.Fragment>
+        <ToastContainer />
+        <NavBar user={user} />
+        <main className="Container">
+          <Switch>
+            <Route path="/register" component={RegisterForm} />
+            <Route path="/login" component={LoginForm} />
+            <Route path="/logout" component={Logout} />
+            <ProtectedRoute path="/movies/:id" component={MovieForm} />
+            <Route
+              path="/movies"
+              render={(props) => <Movies {...props} user={this.state.user} />}
+            />
+            <Route path="/customers" component={Customers}></Route>
+            <Route path="/rentals" component={Rentals}></Route>
+            <Route path="/not-found" component={NotFound}></Route>
+            <Redirect from="/" exact to="/movies" />
+            <Redirect to="/not-found" />
+          </Switch>
+        </main>
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
